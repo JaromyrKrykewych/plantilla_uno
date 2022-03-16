af5ca70a33f393ab2ae17335ae2abd1c4654dcdf
Jaromyr Krykewych <vespinoza.nog@gmail.com>
2022-03-16 12:54:14 -0300

    Mobile

--- js/main.js
+++ js/main.js
@@ -1,12 +1,58 @@
-const nextComment = (e) => {
-    let section = e.target.parentNode.parentNode;
-    let comments = section.children[1].children;
-    for ( i=0 ; i<comments.length ; i++) {
-        if ( comments[i].className == 'active' ){
-            comments[i].classList.toggle('active');
-            comments[i].nextElementSibling.classList.toggle('active');
+(function(){
+    const menu = document.getElementById('menu');
+    const menuNav = document.getElementById('menu-nav');
+    const closeMenu = document.getElementById('close-menu');
+
+    menu.addEventListener('click', () => {
+        menuNav.style.display = 'grid';
+    })
+
+    closeMenu.addEventListener('click', () => {
+        menuNav.style.display = 'none';
+    })
+})()
+
+
+const next = document.getElementById('next');
+const before = document.getElementById('before');
+const comments = document.getElementById('comments');
+
+next.addEventListener('click', (event) => {
+    let commentsArray = [...comments.children];
+    for(i=0; i<commentsArray.length; i++) {
+        if (commentsArray[i].classList.contains('show') && 
+            commentsArray[i] != commentsArray[i].parentNode.lastElementChild
+            ){
+            commentsArray[i].classList.remove('show')
+            commentsArray[i].nextElementSibling.classList.add('show')
+            break
+        } else if (commentsArray[i].classList.contains('show') && 
+            commentsArray[i] == commentsArray[i].parentNode.lastElementChild
+            ){
+            commentsArray[i].classList.remove('show')
+            commentsArray[i].parentNode.firstElementChild.classList.add('show')
+            break
         }
-        console.log(comments)
-        break  
     }
-}
+})
+
+before.addEventListener('click', (event) => {
+    let commentsArray = [...comments.children];
+    for(i=0; i<commentsArray.length; i++) {
+        if (commentsArray[i].classList.contains('show') && 
+            commentsArray[i] != commentsArray[i].parentNode.firstElementChild
+            ){
+            commentsArray[i].classList.remove('show')
+            commentsArray[i].previousElementSibling.classList.add('show')
+            break
+        } else if (commentsArray[i].classList.contains('show') && 
+            commentsArray[i] == commentsArray[i].parentNode.firstElementChild
+            ){
+            commentsArray[i].classList.remove('show')
+            commentsArray[i].parentNode.lastElementChild.classList.add('show')
+            break
+        }
+    }
+})
+
+
